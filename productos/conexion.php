24125cac7fe8faf01aa62caf85a416d47b264192
Azrael229 <israel.navarrete229@gmail.com>
2022-10-01 02:17:50 -0500

    pass y user namedb

--- productos/conexion.php
+++ productos/conexion.php
@@ -1,7 +1,7 @@
 <?php
     
     try{
-        $conexion = new PDO("mysql:host=localhost; port=3306;dbname=servico1_servicomdb", "root", "");
+        $conexion = new PDO("mysql:host=localhost; port=3306; dbname=servico1_servicomdb", "servico1_wp804", "1pBS)5m3!x");
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 
