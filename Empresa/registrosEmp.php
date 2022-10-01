b958a23a5967397956494f7e224cedf047149705
Azrael229 <israel.navarrete229@gmail.com>
2022-10-01 15:06:21 -0500

    se crean las variables con isset POST se crea el if de dato vacio se insertan las variables en los campos de BD

--- Empresa/registrosEmp.php
+++ Empresa/registrosEmp.php
@@ -1,18 +1,25 @@
 <?php
 
-$producto = isset($_POST["Producto"]) ? $_POST["Producto"] : "";
-$descripcion = isset($_POST["descripProducto"]) ? $_POST["descripProducto"] : "";
+$empresa = isset($_POST["Empresa"]) ? $_POST["Empresa"] : "";
+$telefono = isset($_POST["telEmp"]) ? $_POST["telEmp"] : "";
+$direccion = isset($_POST["dirEmp"]) ? $_POST["dirEmp"] : "";
+$ciudad = isset($_POST["cdEmp"]) ? $_POST["cdEmp"] : "";
+$estado = isset($_POST["edoEmp"]) ? $_POST["edoEmp"] : "";
+
     
-    if($producto =="" || $descripcion ==""){
+    if($empresa =="" || $telefono =="" || $direccion =="" || $ciudad =="" || $estado =="" ){
         echo json_encode("false");  
 
     }else{
 
         if(empty($_POST['id'])){
             require ("conexion.php");  
-            $pdo = $conexion->prepare("INSERT INTO productos(Nombre, Descripcion) VALUES(?,?)");
-            $pdo->bindParam(1, $producto);
-            $pdo->bindParam(2, $descripcion);
+            $pdo = $conexion->prepare("INSERT INTO empresas(Nombre, Telefono, Direccion, Ciudad, Estado) VALUES(?,?,?,?,?)");
+            $pdo->bindParam(1, $empresa);
+            $pdo->bindParam(2, $telefono);
+            $pdo->bindParam(3, $direccion);
+            $pdo->bindParam(4, $ciudad);
+            $pdo->bindParam(5, $estado);
             $pdo->execute() or die(print($pdo->errorInfo()));
     
             echo json_encode("true");
